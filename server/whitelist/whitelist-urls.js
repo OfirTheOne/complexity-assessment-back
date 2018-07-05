e0a89aa19fb1100d28d242c64bdc8527138b0012
TheCodeSleyer <ofir3322@gmail.com>
2018-07-05 13:37:07 +0300

    add the front heroku-url

--- server/whitelist/whitelist-urls.js
+++ server/whitelist/whitelist-urls.js
@@ -1,7 +1,8 @@
 
 const whitelist = [
     'http://localhost:4200', 
-    'localhost:4200'
+    'localhost:4200',
+    'https://peaceful-yellowstone-55034.herokuapp.com/'
 ];
 
 module.exports = whitelist;
